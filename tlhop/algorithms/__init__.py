0f64a0a5e73ea821c4776d5218cedf5e85b1e1a2
Lucas Miguel Simoes Ponce <lucasmsp@dcc.ufmg.br>
2023-12-11 15:38:36 -0300

    adding init

---
+++ tlhop/algorithms/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from tlhop.algorithms.fingerprints import Fingerprints
+from tlhop.algorithms.org_enhancement import OrgEnhancement
+
+_all__ = ["Fingerprints", "OrgEnhancement"]
